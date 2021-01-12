1f317cd935f05ae0442d2468f6537e2a824765cc
NilsonVargas <nilson.vargas@uniminuto.edu.co>
2021-01-11 21:28:47 -0500

    Añadida la entidad carta

---
+++ src/app/entities/Card/Card.ts
@@ -0,0 +1,38 @@
+export class Card {
+    private _Ion: String;//Letters or numbers
+    private _figure: String;//Diamonds, Hearts, 
+    private _color: String;
+
+    constructor(Ion: String, figure: String, color: String) {
+        this.Ion = Ion;
+        this._color = color;
+        this._figure = figure;
+    }
+
+    get Ion() {
+        return this.Ion;
+    }
+
+    set Ion(Ion: String) {
+        this.Ion = Ion;
+
+    }
+
+    get Figure() {
+        return this._figure;
+    }
+
+    set Figure(figure: String) {
+        this._figure = figure;
+
+    }
+    get Color() {
+        return this._color;
+    }
+
+    set Color(color: String) {
+        this._color = color;
+
+    }
+
+}
