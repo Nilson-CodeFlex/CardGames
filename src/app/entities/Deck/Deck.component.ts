86a80126162b5b879f6d1a219a4be927c229971a
Santiago <santiagomorafelix@gmail.com>
2021-01-11 21:10:23 -0500

    Estoy pendejo. Salu2.

---
+++ src/app/entities/Deck/Deck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Deck',
+  templateUrl: './Deck.component.html',
+  styleUrls: ['./Deck.component.css']
+})
+export class DeckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/entities/Deck/Deck.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeckComponent } from './Deck.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DeckComponent]
+})
+export class DeckModule { }
